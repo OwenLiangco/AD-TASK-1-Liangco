e6e8f3dc27b78b3bea0e82e092b74a9537b4c094
Owen Liangco <owenliangco21@gmail.com>
2025-05-11 17:04:23 +0800

    feat (home page): added php variables

---
+++ homePage.php
@@ -0,0 +1,7 @@
+<?php
+
+$title = "Top 5 Retro Games I Love";
+$author = "Owen Kyle G. Liangco";
+$intro = "Welcome~! to my mini showcase of my top 5 retro games that shaped my childhood and love for gaming!";
+
+?>
