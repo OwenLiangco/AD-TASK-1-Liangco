50198c74d1d235d36fe4685501402148e8b5e7c7
Owen Liangco <owenliangco21@gmail.com>
2025-05-11 17:05:37 +0800

    feat (home page): added the doctype html

--- homePage.php
+++ homePage.php
@@ -5,3 +5,15 @@ $author = "Owen Kyle G. Liangco";
 $intro = "Welcome~! to my mini showcase of my top 5 retro games that shaped my childhood and love for gaming!";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><? = $title ?></title>
+</head>
+<body>
+    
+</body>
+</html>
